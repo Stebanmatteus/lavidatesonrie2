6e2c8d5495003f33a795c35e5afc9b9603f8af48
Stebanmatteus <36059022+Stebanmatteus@users.noreply.github.com>
2025-04-25 02:28:56 -0600

    Add files via upload

---
+++ res/access.inc.php
@@ -0,0 +1,30 @@
+<?php
+
+// Users data
+$imSettings['access']['webregistrations_gid'] = '3g88j02r';
+$imSettings['access']['users'] = array(
+	'admin' => array(
+		'groups' => array('f67298eh'),
+		'id' => 'f67298eh',
+		'name' => 'Admin',
+		'password' => 'wk209h40',
+		'email' => '',
+		'page' => 'index.html'
+	),
+	'nuevousuario' => array(
+		'groups' => array('qvv8m596'),
+		'id' => 'qvv8m596',
+		'name' => 'Nuevousuario',
+		'password' => '12xhqp5z',
+		'email' => '',
+		'page' => 'index.html'
+	)
+);
+
+// Admins list
+$imSettings['access']['admins'] = array('f67298eh');
+
+// Page/Users permissions
+$imSettings['access']['pages'] = array();
+
+// End of file access.inc.php
